872a228e74124079e28b5724f82fdcf5974a86f1
seungukyu <134684947+seungukyu@users.noreply.github.com>
2025-01-03 15:22:32 +0900

    Update download_llm.py

--- download_llm.py
+++ download_llm.py
@@ -12,27 +12,27 @@ if __name__ == '__main__':
     
     
     ### Llama series
-    if hf_model_type == 'llama3.2-3b':
+    if args.hf_model_type == 'llama3.2-3b':
         model_name = 'meta-llama/Llama-3.2-3B-Instruct'
-    elif hf_model_type == 'llama3.1-8b':
+    elif args.hf_model_type == 'llama3.1-8b':
         model_name = 'meta-llama/Llama-3.1-8B-Instruct'
-    elif hf_model_type == 'llama3-8b':
+    elif args.hf_model_type == 'llama3-8b':
         model_name = 'meta-llama/Meta-Llama-3-8B-Instruct'
-    elif hf_model_type == 'llama3ko-8b':
+    elif args.hf_model_type == 'llama3ko-8b':
         model_name = 'beomi/Llama-3-Open-Ko-8B-Instruct-preview'
         
     ### Qwen series
-    elif hf_model_type == 'qwen2.5-7b':
+    elif args.hf_model_type == 'qwen2.5-7b':
         model_name = 'Qwen/Qwen2.5-7B-Instruct'
-    elif hf_model_type == 'qwen2-7b':
+    elif args.hf_model_type == 'qwen2-7b':
         model_name = 'Qwen/Qwen2-7B-Instruct'
         
     ### EXAONE series
-    elif hf_model_type == 'EXAONE3.5-7.8b':
+    elif args.hf_model_type == 'EXAONE3.5-7.8b':
         model_name = 'LGAI-EXAONE/EXAONE-3.5-7.8B-Instruct'
-    elif hf_model_type == 'EXAONE3-7.8b':
+    elif args.hf_model_type == 'EXAONE3-7.8b':
         model_name = 'LGAI-EXAONE/EXAONE-3.0-7.8B-Instruct'
-    elif hf_model_type == 'None':
+    elif args.hf_model_type == 'None':
         model_name = args.hf_model_name
     
     
